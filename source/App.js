4df4d5772918cced3e61e54ce157a979fb3a15c2
조우현 <woohyunjo@tmon.co.kr>
2017-07-23 11:34:47 +0900

    칸반스타트

--- source/App.js
+++ source/App.js
@@ -1,38 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {render} from 'react-dom';
 import KanbanBoard from './KanbanBoard';
 
-let cardLists = [
-    {
+let cardsList = [
+  {
+    id: 1,
+    title: "Read the Book",
+    description: "I should read the whole book",
+    status: "in-progress",
+    tasks: []
+  },
+  {
+    id: 2,
+    title: "Write some code",
+    description: "Code along with the samples in the book",
+    status: "todo",
+    tasks: [
+      {
         id: 1,
-        title: "Read the Book",
-        description: "I should read the whole book",
-        status: "in-progress",
-        tasks: []
-    },
-    {
+        name: "ContactList Example",
+        done: true
+      },
+      {
         id: 2,
-        title: "Write some code",
-        description: "Code along with the samples in the book",
-        status: "todo",
-        tasks: [
-            {
-                id: 1,
-                name: "ContacList Example",
-                done: true
-            },
-            {
-                id: 2,
-                name: "Kanban Example",
-                done: false
-            },
-            {
-                id: 3,
-                name: "My own experiments",
-                done: false
-            },
-        ]
-    },
-]
+        name: "Kanban Example",
+        done: false
+      },
+      {
+        id: 3,
+        name: "My own experiments",
+        done: false
+      }
+    ]
+  }
+];
 
-render(<KanbanBoard cards={cardList} />, document.getElementById("root"));
+render(<KanbanBoard cards={cardsList} />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
         loaders: [
             {
                 test: /\.jsx?$/,
+                exclude: /node_modules/,
                 loader: 'babel-loader',
                 query: {
                     presets: ['es2015','react']
